7049f3c28f70eec45e9f0be7454e6c03c831e950
ddd42-star <dario.bassi111@gmail.com>
2025-05-09 17:16:16 +0200

    decided to use the FastMCP implementations for the moment

--- src/agentSDK/main_agent.py
+++ src/agentSDK/main_agent.py
@@ -2,27 +2,10 @@ from pydantic import BaseModel
 from agents import Agent
 import asyncio
 import mcp.types as types
-from mcp.server import Server
+from mcp.server import Server, FastMCP
 
-main_agent_server = Server("Main Agent Server")
+main_agent_server = FastMCP("Main Agent Server")
 
-
-@main_agent_server.list_tools()
-async def list_tools() -> list[types.Tool]:
-    return [
-        types.Tool(
-            name="",
-            description="",
-            inputSchema={}
-        )
-    ]
-
-@main_agent_server.call_tool()
-async def call_tool(name: str, arguments: dict) -> list[types.TextContent | types.ImageContent | types.EmbeddedResource]:
-    if name == "tool1":
-        pass
-
-    raise ValueError(f"Tool not found: {name}")
 
 
 
