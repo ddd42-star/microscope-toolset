c640b8825565dc6448c6f9dccaef085160f3096e
ddd42-star <dario.bassi111@gmail.com>
2025-05-15 17:39:26 +0200

    fixed normal chat, fixed thread problem

--- src/gui/napari_microscope_toolset.py
+++ src/gui/napari_microscope_toolset.py
@@ -4,13 +4,9 @@ from mcp_gui import MCPWindow, LLM
 def main():
     # call new napari viewer
     viewer = napari.Viewer()
-
-    #main_window = MCPWindow()
     client = LLM()
-    # Create and show the main window, passing the LLM client
+
     main_window = MCPWindow(client)
-    #main_window.show()
-    # call the plugins
     viewer.window.add_plugin_dock_widget("napari-micromanager")
     viewer.window.add_dock_widget(widget=main_window, name="llm", area="right")
 
