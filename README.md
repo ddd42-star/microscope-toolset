5c20dd4f7d716cead66ffe55ec95c88de441295d
ddd42-star <dario.bassi111@gmail.com>
2025-08-07 19:37:31 +0200

    finished extraction of text from pdf pipeline. It will be saved in a chromadb vector store

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This repository is a toolset for microscope that use pymmcore-plus with LLM
 In order to use this toolset, you need to have python installed. There are differents option, you could use *Anaconda*, *miniconda* or *mamba*. After you installed your favorite package and environment management system create a specific environment for this toolset.
 
 ```
-conda create -n microscope-toolset python=3.12
+conda create -n microscope-toolset python=3.12.11
 ```
 Afterwards activate your newly environment
 ```
--- src/usage/pdf_to_markdown.py
+++ src/usage/pdf_to_markdown.py
@@ -1,14 +1,15 @@
 import asyncio
 import json
 
+import chromadb
 import pymupdf
-import pymupdf4llm
 import os
 import base64
 from openai import OpenAI
 from openai import AsyncOpenAI
 from pydantic import BaseModel
 from pathlib import Path
+from semantic_text_splitter import MarkdownSplitter
 
 #from usage.prompts import CLEAN_MARKDOWN_TEXT
 
@@ -17,10 +18,6 @@ class ImageToMarkdown(BaseModel):
     title: str = None
     page_number: int = None
     author: str = None
-    text: str
-class MarkdownText(BaseModel):
-    title: str
-    author: str
     text: str
 
 
@@ -40,7 +37,7 @@ def load_file(file_path: Path, vector_db_path: str) -> None:
     # convert pdf to an image
     for page in doc:
         pix = page.get_pixmap()
-        pix.save(f"{images_path}/page-%i.png" % page.number)
+        pix.save(f"{images_path}/page_{page.number:03}.png")
 
     # close pdf file
     doc.close()
@@ -59,12 +56,11 @@ async def image_to_markdown(base64_image):
     """
     Convert an image to a Markdown text. Add metadata of the document like 'title', 'page', 'author', ect
     """
-    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
     asynch_client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-    prompt = ("Extract the markdown text output from this page in a PDF using formatting to match the structure of the page as close as you can get. Only output the markdown and nothing else. Do not explain the output, just return it. Do not use a single # for a heading. All headings will start with ## or ### or ####. Convert tables to markdown tables. Describe charts as best you can. DO NOT return in a codeblock. Just return the raw text in markdown format."
-              "Add the metadata required. Some field will be changed only once because are different images of the same PDF.")
+    prompt = ("Extract the text and return it in a markdown format."
+              "DO NOT include figures and figure descriptions, publication information and pages number, title and author.")
     response = await asynch_client.responses.parse(
-        model="gpt-4.1",
+        model="gpt-4.1-mini",
         input=[
             {
                 "role": "user",
@@ -100,7 +96,6 @@ async def extract_text_from_pdfs(path_for_the_db: str = None, pdfs_path: str = N
     try:
         input_db_dir = os.path.abspath(path_for_the_db).replace("\\", "/")
         input_pdfs_dir = os.path.abspath(pdfs_path).replace("\\", "/")
-        output_db_dirt = "to add"
 
         if not os.path.isdir(input_db_dir):
             raise ValueError("Please give a value directory path to save the vector database")
@@ -111,15 +106,21 @@ async def extract_text_from_pdfs(path_for_the_db: str = None, pdfs_path: str = N
 
         # Get all pdfs from the path
         pdfs_inputs = sorted(Path(input_pdfs_dir).glob("*.pdf"), key=lambda path: path.stem)
+        print(pdfs_inputs)
         # iterate throught all the files
         for markdown_file_path in pdfs_inputs:
             print(f"Processing {markdown_file_path}")
+
+            # create folder for each publication
+            pdf_file_name = markdown_file_path.name.split('.')[0]
+            folder_name = f"{input_db_dir}/{pdf_file_name}"
+            if not os.path.exists(folder_name):
+                os.makedirs(folder_name)
             # extract text from single pdf
             #markdown_object_single_file = await extract_text_from_pdf(input_db_dir,markdown_file_path)
-            #print(markdown_file_path)
-            #print(markdown_file_path.name)
-            #print(markdown_object_single_file)
-            print(markdown_file_path.absolute())
+            print(f"Starting with {pdf_file_name}")
+            await extract_text_from_pdf(folder_name, markdown_file_path)
+            print(f"Finished with {pdf_file_name}")
 
 
 
@@ -139,106 +140,109 @@ async def extract_text_from_pdf(path_for_the_db: str = None, pdf_path: Path = No
     # get path name
     pages_folder_vector_database = format_vector_database + '/pages_png'
     markdown_folder_vector_database = format_vector_database + '/pages_markdown'
-    pymupdf4llm_folder_vector_database = format_vector_database + '/pymupdf4llm_markdown'
 
     # create markdown directory
     if not os.path.exists(markdown_folder_vector_database):
         os.makedirs(markdown_folder_vector_database)
-    # create other dir
-    if not os.path.exists(pymupdf4llm_folder_vector_database):
-        os.makedirs(pymupdf4llm_folder_vector_database)
+
+    if not os.path.exists(pages_folder_vector_database):
+        os.makedirs(pages_folder_vector_database)
+
     multimodal_pages = []
-    pymupdf4llm_pages = []
 
     # checks what type of path is: file or directory or unknown
     if os.path.isfile(pdf_path):
-        # get file name
-        file_name = os.path.abspath(pdf_path).replace("\\", "/")
         # convert pdf to img PIL
         try:
             load_file(pdf_path, format_vector_database)
+
+            # access image files
+            images = sorted(Path(pages_folder_vector_database).iterdir(), key=lambda x: x.stem)
+            for image_path in images:
+                print(f"Process {image_path}")
+                base64_image = await encode_image_to_base64(str(image_path))
+                markdown_object = await image_to_markdown(base64_image)
+                output_markdown = Path(markdown_folder_vector_database) / f"{image_path.stem}.md"
+
+                # add the Markdown object to the list
+                multimodal_pages.append(markdown_object)
+
+                # write raw Markdown text from pages
+                with open(output_markdown, 'w', encoding='utf-8') as file:
+                    file.write(markdown_object['text'])
+                    print(f"Markdown for {image_path.name} saved to {output_markdown}")
+
+                file.close()
+
+
+            unique_markdown_text = '\n'.join([page['text'] for page in multimodal_pages])#cleaned_markdown_list
+            with open(Path(vector_database) / f"{pdf_path.name.split('.')[0]}.md", 'w', encoding='utf-8') as file:
+                file.write(unique_markdown_text)
+
+            file.close()
+
+            # create a list with all the chunk, as default will be 5000 charachter
+            print("Create chunks list")
+            chunks_list = split_markdown(unique_markdown_text, 5000)
+
+            # insert chuncked list into the vector db
+            print("Insert chunks into the database")
+            insert_chunks_into_collection(pdf_path.name.split('.')[0], chunks_list, pdf_path)
+
         except Exception as e:
+            print(e)
             raise e
-        # access image files
-        images = sorted(Path(pages_folder_vector_database).iterdir(), key=lambda x: x.stem)
-        for image_path in images:
-            print(f"Process {image_path}")
-            base64_image = await encode_image_to_base64(str(image_path))
-            markdown_object = await image_to_markdown(base64_image)
-            output_markdown = Path(markdown_folder_vector_database) / f"{image_path.stem}.md"
-
-            # add the Markdown object to the list
-            multimodal_pages.append(markdown_object)
-
-            # write raw Markdown text from pages
-            with open(output_markdown, 'w', encoding='utf-8') as file:
-                file.write(markdown_object['text'])
-                print(f"Markdown for {image_path.name} saved to {output_markdown}")
-
-            file.close()
-
-        # # extract the markdown text using pymupdf4llm
-        # # extract Markdown text using pymupdf4llm
-        # docs = pymupdf.open(pdf_path)
-        # for page in docs:
-        #     print(f"Process {page}")
-        #     page_text = pymupdf4llm.to_markdown(doc=pdf_path, pages=[page.number])
-        #     # write the file
-        #     Path(f"{pymupdf4llm_folder_vector_database}/page-%i.md" % page.number).write_bytes(page_text.encode())
-        #     # add to the list
-        #     infos = {
-        #         "page": page.number,
-        #         "text": page_text
-        #     }
-        #     pymupdf4llm_pages.append(infos)
-        cleaned_markdown_list = []
-        # # Use both markdown object to create the final one
-        for i, _ in enumerate(multimodal_pages):
-            cleaned_markdown_text = await clean_markdown(multimodal_pages[i], pymupdf4llm_pages)
-            cleaned_markdown_list.append(cleaned_markdown_text)
-            with open(f"{pymupdf4llm_folder_vector_database}/page-{i}.md", 'w', encoding='utf-8') as file:
-                file.write(cleaned_markdown_text['text'])
-            file.close()
-        #cleaned_markdown_text = await clean_markdown(multimodal_pages, pymupdf4llm_pages)
-
-        # create unique Markdown doc
-        unique_markdown_text = ''.join([page['text'] for page in cleaned_markdown_list])
-        with open(Path(vector_database) / f"{pdf_path.name.split('.')[0]}.md", 'w', encoding='utf-8') as file:
-            file.write(unique_markdown_text)
-            #file.write(cleaned_markdown_text['text'])
-
-        file.close()
-
     else:
         raise FileNotFoundError("The path to the pdfs doesn't exists!")
 
+def split_markdown(text, max_char = 1000):
 
-async def clean_markdown(multimodal_object: dict, pymupdf_object: list):
-    asynch_client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-    #prompt = CLEAN_MARKDOWN_TEXT
-    #sections = '\n'.join([page['text'] for page in multimodal_object])
-    prompt = """
-You are tasked with cleaning up the following markdown text. You should return only the cleaned up markdown text. Do not explain your output or reasoning. \n remove any irrelevant text from the markdown, returning the cleaned up version of the content. Examples include any images []() or 'click here' or 'Listen to this article' or page numbers or logos.
-"""
-    text = """
-    # TEXT TO CLEAN
-    {multimodal_text}
+    # Maximum number of characters in a chunk
+    max_characters = max_char
+    # Optionally can also have the splitter not trim whitespace for you
+    splitter = MarkdownSplitter(max_characters)
+
+    chunks = splitter.chunks(text)
+
+    return chunks
+
+def get_openai_embeddings(text: str):
     """
-    format_text = text.format(multimodal_text=multimodal_object['text'])
-    response = await asynch_client.responses.parse(
-        model="gpt-4.1-mini",
-        input=[{"role": "system", "content": prompt}, {"role": "user", "content": format_text}],
-        text_format=MarkdownText
-    )
+    This function return the embedding vector calculated by OpenAI
+    """
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    response = client.embeddings.create(input=text, model="text-embedding-3-small", dimensions=512)
 
-    # convert to a json object
-    response_json = json.loads(response.output_text)
+    embedding = response.data[0].embedding
+    print(embedding)
+    print("Generating embedding")
 
-    return response_json
+    return embedding
+
+
+def insert_chunks_into_collection(file_name: str, chuncked_list: list, pdf_path: Path):
+    chroma_client = chromadb.PersistentClient("C:/Users/dario/OneDrive/università/MA/Thesis/microscope-toolset/microscope-toolset/Vectore-store")
+
+    collection_name = chroma_client.get_or_create_collection(name="publication-database")
+
+    for index, chunk in enumerate(chuncked_list):
+        print(f"chunk: {index}_{chunk}")
+        # create embedding
+        embedding_vector = get_openai_embeddings(chunk)
+
+        # create random id
+        id_name = f"{file_name}_{index:04}"
+        # insert into the collection
+        collection_name.upsert(ids=[id_name], metadatas=[{"name": file_name, "path": f"{pdf_path}"}], documents=[chunk], embeddings=[embedding_vector])
+
+    print(f"{file_name} chuncks was inserted into {collection_name}!")
+
+    return None
+
 
 
 if __name__ == "__main__":
-    asyncio.run(extract_text_from_pdf(path_for_the_db="./new_publication", pdf_path=Path("C:\\Users\\dario\\OneDrive\\università\\MA\\Thesis\\microscope-toolset\\microscope-toolset\\publications\\publication_1.pdf")))
+    #asyncio.run(extract_text_from_pdf(path_for_the_db="./new_publication", pdf_path=Path("C:\\Users\\dario\\OneDrive\\università\\MA\\Thesis\\microscope-toolset\\microscope-toolset\\publications\\publication_1.pdf")))
 
-    # asyncio.run(extract_text_from_pdfs(path_for_the_db="./new_publication",
-    #                                    pdfs_path="C:\\Users\\dario\\OneDrive\\università\\MA\\Thesis\\microscope-toolset\\microscope-toolset\\publications"))
+     asyncio.run(extract_text_from_pdfs(path_for_the_db="./new_publication",
+                                        pdfs_path="C:\\Users\\dario\\OneDrive\\università\\MA\\Thesis\\microscope-toolset\\microscope-toolset\\test_pipeline_publication"))
